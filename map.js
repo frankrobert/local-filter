69f6ffd9c6106117f08f0b9a68b324283941340e
officialphrank <frankie.robert@gmail.com>
2016-01-01 17:34:30 -0500

    adjusted the styling of the list, used pagination.b == null to attempt to query the last page of the list that had no hasNextPage == true result... added more to the LIST view but nothing additional to the map / geoResultAddress array.... strange!

--- map.js
+++ map.js
@@ -26,9 +26,9 @@ var initMap = function() {
 	//	});
 
 	// event listener to close any open infowindows on map click
-		map.addListener('click', function(event) {
-			infoWindow.close();
-		});
+	map.addListener('click', function(event) {
+		infoWindow.close();
+	});
 
 	// Adds a marker at the center of the map.
 
@@ -75,48 +75,60 @@ function callback(results, status, pagination) {
 		console.error(status);
 		return;
 	}
-	for (var i = 0, result; result = results[i]; i++) {
-		if (result.name.toLowerCase().indexOf('station') === 0) {
-			addMarker(result);
-			geoResultAddress.push(result);
-		}
-	}
 	if (pagination.hasNextPage) {
+
+		for (var i = 0, result; result = results[i]; i++) {
+			if (result.name.toLowerCase().indexOf('station') === 0) {
+				addMarker(result);
+				geoResultAddress.push(result);
+			}
+		}
 		sleep: 2;
 		pagination.nextPage();
+	}
+	if (pagination.b == null) {
+		for (var i = 0, result; result = results[i]; i++) {
+			if (result.name.toLowerCase().indexOf('station') === 0) {
+				addMarker(result);
+				geoResultAddress.push(result);
+			}
+		}
 	}
 }
 
 function addMarker(place) {
-	var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location,
-		animation: google.maps.Animation.DROP,
-		icon: {
-			url: 'underground.png',
-			anchor: new google.maps.Point(10, 10),
-			scaledSize: new google.maps.Size(15, 22)
-		}
-	});
-	/*
-		service.getDetails(place, function(result, status) {
-			if (status !== google.maps.places.PlacesServiceStatus.OK) {
-				console.error(status);
-				return;
+	var delay = 1000;
+	setTimeout(function() {
+		var marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location,
+			animation: google.maps.Animation.DROP,
+			icon: {
+				url: 'underground.png',
+				anchor: new google.maps.Point(10, 10),
+				scaledSize: new google.maps.Size(15, 22)
 			}
-	//		geoResultAddress.push(result);
 		});
-	*/
-	google.maps.event.addListener(marker, 'click', function() {
-		service.getDetails(place, function(result, status) {
-			if (status !== google.maps.places.PlacesServiceStatus.OK) {
-				console.error(status);
-				return;
-			}
-			infoWindow.setContent(result.name, result.location);
-			infoWindow.open(map, marker);
+		/*
+			service.getDetails(place, function(result, status) {
+				if (status !== google.maps.places.PlacesServiceStatus.OK) {
+					console.error(status);
+					return;
+				}
+		//		geoResultAddress.push(result);
+			});
+		*/
+		google.maps.event.addListener(marker, 'click', function() {
+			service.getDetails(place, function(result, status) {
+				if (status !== google.maps.places.PlacesServiceStatus.OK) {
+					console.error(status);
+					return;
+				}
+				infoWindow.setContent(result.name, result.location);
+				infoWindow.open(map, marker);
+			});
 		});
-	});
+	}, delay);
 }
 
 // LOOP STYLE CALLBACK - reuse if thought to be needed.
